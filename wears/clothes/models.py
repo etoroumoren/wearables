bfd80f01a877a2b4056b96bdedc91bb18a79f5e9
torohkiss <torohkiss@yahoo.com>
2025-09-10 23:56:20 +0100

    added templates

--- wears/clothes/models.py
+++ wears/clothes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -16,6 +17,11 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse(
+            'clothes:product_list_by_category', args=[self.slug]
+        )
     
     
     
@@ -47,3 +53,9 @@ class Product(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse(
+            'clothes:product_detail', args=[self.id, self.slug]
+        )
+    
